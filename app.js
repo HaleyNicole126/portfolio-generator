ff4ed6fb682e7b2a7ead4fe62a98dfb5f73431f8
haybales <haleynicole126@gmail.com>
2022-02-16 13:48:49 -0700

    interface with inquirer

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
@@ -20,7 +28,7 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
 // Notice the lack of parentheses around the 'profileDataArr' parameter
 // const printProfileData = profileDataArr => {
-//     // this.. 
+//     // this..
 //     for (let i = 0; i < profileDataArr.length; i+=1) {
 //     console.log(profileDataArr[i]);
 //     }
