59c8c43311073b5b3b923015d84b27b22329405f
haybales <haleynicole126@gmail.com>
2022-05-08 13:49:34 -0600

    updated template

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -74,14 +76,19 @@ const promptProject = portfolioData => {
         });
 };
 
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
-    });
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./dist/index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
+
 
 // const pageHTML = generatePage(name, github);
 
